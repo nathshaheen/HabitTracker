2be831eacdb7b417efd00eb38fa25906e6646828
Nathan Shaheen <nathshaheen1@gmail.com>
2023-03-17 20:00:00 +1100

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Nathan Shaheen on 17/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
